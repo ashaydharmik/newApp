923b17f686a235e8d3af0ac4da74602f251e94c8
Ashay <101341591+ashaydharmik@users.noreply.github.com>
2023-04-02 12:58:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Home from './Home'
 
-const App = () => {
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './Home';
+
+export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Home />
+
     </View>
-  )
+  );
 }
 
-export default App
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
